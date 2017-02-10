05f783c7d1b69d5d1cad09a86880eac8f27ece16
Nitin Rane <trulyspecialone@gmail.com>
2017-02-10 07:29:10 +0000

    terraform test successful, need to add security groups

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 provider "aws" {
-access_key = "$(var.access_key)"
-secret_key = "$(var.secret_key)"
-region = "$(var.region)"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-70edb016"
+  instance_type = "t2.micro"
+  key_name      = "terraform"
+  
+  tags {
+  name = "terraform_example"
+  }
 }
